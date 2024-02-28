b9bf53aec6f90537ffbadcb87758e8e1f3fac8e3
tumursukh.ba <tumursukh.ba@nomin.net>
2024-02-28 17:22:38 +0800

    added more of form

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+
+import { db } from "@/lib/db";
 
 // import { revalidatePath, revalidateTag  } from "next/cache";
 
@@ -16,7 +19,28 @@ export const register = async ( values: z.infer<typeof RegisterSchema> ) => {
 
    if( !validatedFields.success) {
         return { error: "Invalid fields!"};
+    }
 
+   const { email, name, password} = validatedFields.data;
+   const hashedPassword = await bcrypt.hash(password, 10);
+
+   const existingUser = await db.user.findUnique({
+    where: { email},
+   });
+
+   if (existingUser) {
+    return { error: "Имэйл хаяг бүртгэгдсэн байна!"}
    }
-   return { success: "Email Sent"}
+    
+   await db.user.create ({
+    data: {
+        name, 
+        email, 
+        password: hashedPassword,
+    }
+   });
+
+   // TODO: send verification token email
+
+   return { success: "Шинэ хэрэглэгчээр амжилттай бүртгэлээ!"}
 }
